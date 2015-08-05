5cecb223ec2a824142ebe26174ab958fad965319
vmvinothkumar <vmvinothkumar@gmail.com>
2015-08-05 15:41:50 +0530

    prachana

---
+++ botsmani/botsmani/AllowViewController.h
@@ -0,0 +1,13 @@
+//
+//  AllowViewController.h
+//  botsmani
+//
+//  Created by vmvinothkumar on 05/08/15.
+//  Copyright (c) 2015 vmvinothkumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AllowViewController : UIViewController
+
+@end
